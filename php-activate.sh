ad037f80a9335516b55d76512d2278622141c27b
Sander van Dragt <sander@vandragt.com>
2023-02-08 10:21:23 +0000

    fix corrupted output in direnv

--- php-activate.sh
+++ php-activate.sh
@@ -6,16 +6,16 @@ function php-activate {
         local VERSIONS=$(update-alternatives --query php | grep Alternative | cut -d ' ' -f2 )
         if echo $VERSIONS | grep -q "$1"
         then
-            export PHP_PICKED=$(echo $VERSIONS | sort | grep "$1" | head -n 1)
+            export PHP_PICKED=$(echo "$VERSIONS" | sort | grep "$1" | head -n 1)
             function php {
                 $PHP_PICKED $@
             }
             echo "Activated: $PHP_PICKED"
-            php --version
+            php --version | grep built
             hash -r
         else
             echo "Error: No matching PHP version found. Use one of the following:"
-            echo $VERSIONS
+            echo "$VERSIONS"
         fi
         return
     fi
@@ -25,7 +25,7 @@ function php-activate {
         echo "Deactivated: $PHP_PICKED"
         unset PHP_PICKED
         unset -f php
-        php --version
+        php --version | grep built
         hash -r
         return
     fi
